2c4b2c7003fd53fb7ed949120a0767b2fca875c3
Rolando Valle <lancevalle8@gmai.com>
2025-02-08 22:33:15 +0800

    feat: add navigation menu with styling and functionality
    - Implement navigation context (logo, menu, nav/close icon)
    - Add responsive styles using media queries
    - Add JavaScript for opening and closing the navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,35 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
 
+// Menu Show
+// validate if constant exist
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+// Menu Hiddent
+if(navClose){
+    navClose.addEventListener('click',()=> {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu =  document.getElementById('nav-menu')
+    //When click on each nav__link, we remove the show menu class
+    navMenu.classList.remove('show-menu')
+}
 
+navLink.forEach(n => n.addEventListener('click',linkAction))
 /*=============== SHADOW HEADER ===============*/
 
 
