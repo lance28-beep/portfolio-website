0db2853c67341661bbd9bfdd874d53641134871d
Rolando Valle <lancevalle8@gmai.com>
2025-02-11 23:07:58 +0800

    feat: integrate email functionality using EmailJS
    - Implement EmailJS for sending emails from the contact form
    - Configure EmailJS service and template
    - Add form submission handling with JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,33 @@ window.addEventListener('scroll',shadowHeader)
 
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+
+    e.preventDefault()
+
+    //ServiceID - template - #form - publicKey
+    emailjs.sendForm('service_itxfpq8','template_xlr53ge','#contact-form','1MPWDzeALq9L05koW')
+    .then(() => {
+        // show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        //Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        },5000)
+
+        //Clear input fields
+        contactForm.reset()
+    }, () => {
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
