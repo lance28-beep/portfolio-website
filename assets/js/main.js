90f7dafd39f40894a8aa76fac5ada75de56c1a77
Lance Valle <vallelance8@gmail.com>
2025-02-12 18:42:06 +0800

    feat: add scroll reveal animation
    - Implement scroll animation for elements on viewport entry
    - Enhance user experience with smooth fade-in effects
    - Optimize animations for performance

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,3 +138,16 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+    //reset: true // Animations repeat
+})
+
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin:'right'})
+sr.reveal(`.home__name, .home__info, .contact__social,
+         .about__container, .section__title-1, .section__title-2, .about__info,
+            contact__social, .contact__data`, {origin:'left'})
+sr.reveal(`.services__card, .projects__card`, {interval:100})
